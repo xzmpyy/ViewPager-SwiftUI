a22e7db03da8045c40578ab4d137fd8f62356aee
xzmpyy <zj0714zj@163.com>
2020-06-30 08:51:20 +0800

    Initial Commit

---
+++ ViewPager-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewPager-SwiftUI
+//
+//  Created by 张捷 on 2020/6/30.
+//  Copyright © 2020 张捷. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
